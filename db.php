e2d126f2867de353cc6afdcec6f07dd1100f0b1c
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2016-12-10 23:37:47 +0200

    Delete db.php

--- db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'webacademy', 'webacademy');
-$db = mysqli_select_db($conn, 'webacademy');
-
-if(mysqli_connect_errno()){
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit;
-}
