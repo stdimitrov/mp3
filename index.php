73405ddb07d52b6adf5df295b9bae461b4ce78fe
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-01-31 23:52:12 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+mb_internal_encoding('UTF-8');
+include 'html/dct.php';
+include 'html/header.php';
+?>
+
+<menu id="nav">
+    <li><a href="index">Начало 1</a></li>
+    <li><a href="str2">Начало 2</a></li>
+    <li><a href="str3">Начало 3</a></li>
+    <li><a href="str4">Начало 4</a></li>
+    <li><a href="str5">Начало 5</a></li>
+</menu>
+
+<div id="content">
+    <div>
+
+        <?php
+        include 'html/footer.php';
+        ?>
