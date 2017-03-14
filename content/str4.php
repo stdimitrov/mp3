decb6add226b32257748c611c21cfbbff9bcb2db
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-03-14 23:16:06 +0200

    Update str4.php

--- content/str4.php
+++ content/str4.php
@@ -1,1 +1,42 @@
-<h2>stranica 4</h2>
+<h2>Проекти</h2>
+
+<script>
+
+    $("#slideshow > div:gt(0)").hide();
+
+    setInterval(function () {
+        $('#slideshow > div:first')
+            .fadeOut(3000)
+            .next()
+            .fadeIn(3000)
+            .end()
+            .appendTo('#slideshow');
+    }, 10000);
+
+</script>
+<div id="slideshow">
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d1.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d2.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d3.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d4.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d5.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d6.jpeg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d7.jpg" width="600" height="420"/>
+    </div>
+    <div class="slideshow_img">
+        <img class="img" src="picture/project/d8.jpg" width="600" height="420"/>
+    </div>
+</div>
