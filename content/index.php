c548d0d7aadc3926e54032727fb08917d01691ac
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-03-14 23:13:16 +0200

    Update index.php

--- content/index.php
+++ content/index.php
@@ -1,6 +1,5 @@
-
-<h2>stranica 1</h2>
 <div id="page1">
+    <h2>За нас</h2>
     <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
         labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
