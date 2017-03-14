ac17f33e23aed173254dc66455429ec6100f6be3
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-03-14 23:14:43 +0200

    Update str3.php

--- content/str3.php
+++ content/str3.php
@@ -1,32 +1,34 @@
-<h2>stranica 3</h2>
-
 <script>
-    $(document).ready(function(){
-        $("#slide-a").click(function(){
+
+    $(document).ready(function () {
+        $("#slide-a").click(function () {
             $("#panel-a").toggle("slow");
         });
-        $("#slide-b").click(function(){
+        $("#slide-b").click(function () {
             $("#panel-b").toggle("slow");
         });
-        $("#slide-c").click(function(){
+        $("#slide-c").click(function () {
             $("#panel-c").toggle("slow");
         });
-        $("#slide-d").click(function(){
+        $("#slide-d").click(function () {
             $("#panel-d").toggle("slow");
         });
     });
+
 </script>
 
 <div id="slide-a">Първи слайд</div>
 <div id="panel-a">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
     sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
     Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-    aliquip ex ea commodo consequat.</div>
+    aliquip ex ea commodo consequat.
+</div>
 
 <div id="slide-b">Втори слайд</div>
 <div id="panel-b">Duis aute irure dolor in reprehenderit in voluptate velit esse
     cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</div>
 
 <div id="slide-c">Трети слайд</div>
 <div id="panel-c">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
@@ -36,10 +38,12 @@
     sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
     Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
     adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et
-    dolore magnam aliquam quaerat voluptatem.</div>
+    dolore magnam aliquam quaerat voluptatem.
+</div>
 
 <div id="slide-d">Четвърти слайд</div>
 <div id="panel-d">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
     praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi
     sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia
-    animi, id est laborum et dolorum fuga.</div>
+    animi, id est laborum et dolorum fuga.
+</div>
