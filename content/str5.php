bccff478acae2c0c6c1e157ac76ca3a2ca5c2b80
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-03-14 23:16:34 +0200

    Update str5.php

--- content/str5.php
+++ content/str5.php
@@ -1,3 +1,24 @@
-<h2>stranica 5</h2>
+<?php
+if (!empty($msg)) {
+    echo "<h2>$msg</h2>";
+}
+?>
 
-<p>Изпрати емейл.</p>
+<div id="sender">
+
+    <h2>Изпратете email</h2>
+
+    <form method="POST" action="function.php">
+        <div id="name">
+            <label for="name"><input type="text" placeholder="Име" name="name" id="name-name"></label>
+        </div>
+        <div id="email">
+            <label for="email"><input type="email" placeholder="Email адрес" name="email" id="email-email"></label>
+        </div>
+        <div id="message">
+            <label for="message"><textarea class="message" name="message" placeholder="Съобщение" id="message-message"
+                                           rows="8" cols="20"></textarea></label>
+        </div>
+        <input type="submit" id="button" value="Изпрати">
+    </form>
+</div>
