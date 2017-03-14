3b15a1fd3294be1b1afe7e500d09f040868bd95e
Stanimir Dimitrov <stdimitrov@users.noreply.github.com>
2017-03-14 23:14:13 +0200

    Update str2.php

--- content/str2.php
+++ content/str2.php
@@ -1,5 +1,149 @@
-<h2>stranica 2</h2>
-
 <div id="page2">
-    
+
+    <h2>Екип</h2>
+
+    <div id="box-left">
+        <img src="picture/crew/b6.png">
+        <ul>
+            <li><p>Име:</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="90" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="90" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="80" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="93" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; example@example.com</p></li>
+        </ul>
+    </div>
+    <div id="box-right">
+        <img src="picture/crew/b7.jpg">
+        <ul>
+            <li><p>Име: Darth Vader</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="87" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="90" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="73" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="92" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; Death_Star@DarthSite.com</p></li>
+        </ul>
+    </div>
+    <div id="box-left">
+        <img src="picture/crew/b3.png">
+        <ul>
+            <li><p>Име:</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="55" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="58" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="65" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="70" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; example@example.com</p></li>
+        </ul>
+    </div>
+    <div id="box-right">
+        <img src="picture/crew/b4.png">
+        <ul>
+            <li><p>Име:</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="50" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="50" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="63" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="60" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; example@example.com</p></li>
+        </ul>
+    </div>
+    <div id="box-left">
+        <img src="picture/crew/b1.png">
+        <ul>
+            <li><p>Име:</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="25" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="40" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="40" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="32" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; example@example.com</p></li>
+        </ul>
+    </div>
+    <div id="box-right">
+        <img src="picture/crew/b5.png">
+        <ul>
+            <li><p>Име:</p></li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="2" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="7" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="6" max="100"></progress>
+            </li>
+            <li><p>&#x25BA; Квалификация</p></li>
+            <li>
+                <progress value="5" max="100"></progress>
+            </li>
+            <li><p>&#9990; +359</p></li>
+            <li><p>&#x40; example@example.com</p></li>
+        </ul>
+    </div>
 </div> 
